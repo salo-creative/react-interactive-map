6f0c240057b7dfcac078b56f2eabb8107c322ffa
SaloCreative <rich@salocreative.co.uk>
2019-01-31 16:48:58 +0000

    Getting basic location grouping working

--- src/mercator/_story.js
+++ src/mercator/_story.js
@@ -6,12 +6,58 @@ import { withKnobs, object, boolean, text, number } from '@storybook/addon-knobs
 import { withTests } from '@storybook/addon-jest';
 import withReadme from 'storybook-readme/with-readme';
 import { H2, Column, P, Row, colours } from '@salocreative/ui';
+import { indexOf } from 'lodash';
 
 import results from '../../.storybook/jest-test-results.json';
 import MercatorMap from './index';
 
 // README //
 import README from '../../README_MERCATOR.md';
+import locations from './locations.json';
+
+const calculateGroupPosition = (group) => {
+  const lat = group.reduce((acc, item) => acc + item.lat, 0) / group.length;
+  const lon = group.reduce((acc, item) => acc + item.lon, 0) / group.length;
+  return { lat, lon };
+};
+
+const cleanedLocations = (zoom = 1) => {
+  const mappedLocations = [];
+  return locations.map(location => {
+  // Check if location already grabbed as part of an
+  // earlier grouping and only proceed if not grouped
+    const alreadyMapped = indexOf(mappedLocations, location);
+    console.log(zoom);
+    const latResolution = zoom ? (2.5 / zoom) : 2.5;
+    const lonResolution = zoom ? (5 / zoom) : 5;
+  
+    if (alreadyMapped === -1) {
+      const groupedLocations = [location];
+      locations.forEach(item => {
+        if (item !== location) {
+        // CHECK IF LOCATION COLLIDES WITH ANOTHER
+          const latCollision = location.lat - item.lat < latResolution && location.lat - item.lat > -latResolution;
+          const lonCollision = location.lon - item.lon < lonResolution && location.lon - item.lon > -lonResolution;
+          if (latCollision && lonCollision) {
+          // EXCLUDE FROM A LATER MAP AND STORE
+            mappedLocations.push(item);
+            groupedLocations.push(item);
+          }
+        }
+      });
+
+      const { lat, lon } = calculateGroupPosition(groupedLocations);
+
+      return {
+        lat,
+        lon,
+        name: location.name,
+        locations: groupedLocations
+      };
+    }
+    return null;
+  }).filter(i => i);
+};
 
 const stories = storiesOf('Mercator Map', module);
 stories.addDecorator(withReadme(README));
@@ -43,17 +89,17 @@ stories.add(
   () => {
     const hideAntarctica = boolean('Hide Antarctica', true);
     const baseColor = text('Base color', '#cccccc');
-    const locations = object('Locations', [
-      { label: 'LDN', lat: 51.5074, lon: -0.1278 },
-      { label: 'NY', lat: 40.7128, lon: -74.0060 },
-      { label: 'AUK', lat: -36.848, lon: 174.7633 },
-      { label: 'AMS', lat: 52.3680, lon: 4.9036 },
-      { label: 'CAT', lat: -33.9249, lon: 18.4241 },
-      { label: 'PAN', lat: 8.5380, lon: -80.7821 },
-      { label: 'RIO', lat: -22.9068, lon: -43.1729 }
-    ]);
+    // const locations = object('Locations', [
+    //   { label: 'LDN', lat: 51.5074, lon: -0.1278 },
+    //   { label: 'NY', lat: 40.7128, lon: -74.0060 },
+    //   { label: 'AUK', lat: -36.848, lon: 174.7633 },
+    //   { label: 'AMS', lat: 52.3680, lon: 4.9036 },
+    //   { label: 'CAT', lat: -33.9249, lon: 18.4241 },
+    //   { label: 'PAN', lat: 8.5380, lon: -80.7821 },
+    //   { label: 'RIO', lat: -22.9068, lon: -43.1729 }
+    // ]);
     const center = object('Center', { lat: 51.5074, lon: -0.1278 });
-    const zoom = number('Zoom', 12);
+    const zoom = number('Zoom', 1);
     return (
       <React.Fragment>
         <Row>
@@ -64,22 +110,29 @@ stories.add(
               zoom={ zoom }
               center={ center }
             >
-              { ({ evalCoordinates }) => locations.map(location => {
-                const { lat, lon, label } = location;
+              { ({ evalCoordinates }) => cleanedLocations(zoom).map(location => {
+                const { lat, lon, name, locations } = location;
                 const coords = evalCoordinates({ lat, lon });
                 return (
                   <span
-                    key={ label }
+                    key={ name }
                     style={ {
-                      height: '6px',
-                      width: '6px',
+                      height: '15px',
+                      width: '15px',
                       borderRadius: '50%',
                       background: 'red',
                       position: 'absolute',
-                      left: `calc(${ coords.x }% - 3px)`,
-                      top: `calc(${ coords.y }% - 3px)`
+                      left: `calc(${ coords.x }% - 7.5px)`,
+                      top: `calc(${ coords.y }% - 7.5px)`,
+                      color: '#fff',
+                      fontSize: '9px',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center'
                     } }
-                  />
+                  >
+                    { locations.length }
+                  </span>
                 );
               }) }
             </MercatorMap>
